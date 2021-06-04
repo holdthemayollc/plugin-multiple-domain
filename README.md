09eb041afa14d5c2429e5c2f2223a6c366de5da6
Joe Wanko <yo@joewanko.com>
2021-06-03 21:09:10 -0400

    Updating Repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# plugin-multiple-domain
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.6',
+    'version' => '1.0.6.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'htmllc/plugin-multiple-domain',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+    ),
+    'htmllc/plugin-multiple-domain' => 
+    array (
+      'pretty_version' => '1.0.6',
+      'version' => '1.0.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+      'provided' => 
+      array (
+        0 => '1.0.6',
+      ),
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
